4f46ed1f5d0997f7c8233688368aa91a2a6969b3
asifmohd94 <asf.mohd@gmail.com>
2021-04-26 18:51:57 +0530

    basic calculator api

--- calculator-api/index.js
+++ calculator-api/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const calc = require('./routes/calc');
-
-const port=3000;
+const bodyPraser=require('body-parser');
+const port = 3000;
 const app = express();
 
 app.use(express.json())
-app.use('/api/calc',calc);
+app.use('/api/calc', calc);
 
 
-app.listen(port,(err)=>{
-if(err){
-    console.log(`Error in running server`);
-}
-console.log(`Server is running on port ${port}`);
-})
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`Error in running server`);
+    }
+    console.log(`Server is running on port ${port}`);
+});
--- calculator-api/routes/calc.js
+++ calculator-api/routes/calc.js
@@ -1,220 +1,103 @@
 const express = require('express');
+
 const router = express.Router();
 
-const operations = [
-    {
-        id: 1,
-        operands: [2,3,1],
-        result: 0,
-        operationPerformed: ""
+const history = [];
 
-    },
-    {
-        id: 2,
-        operands: [1,2,5],
-        result: 0,
-        operationPerformed: ""
-    }
-]
-
-const history=[];
-
-let findLargest = (operations) => {
-    let large = -Infinity;
-    for (let i of operations) {
-        if (i.id > large) {
-            large = i.id;
-        }
-    }
-    return large;
-}
-
-router.get('/add', (req, res) => {
-    for (let x = 0; x < operations.length; x++) {
-        let arr = operations[x].operands;
-        let sum = 0;
-        for (let i = 1; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        operations[x].result = sum;
-        operations[x].operationPerformed="+"
-    }
-    res.send(operations);
-})
-
-
-router.get('/add/:id',(req,res)=>{
-     let paramId= parseInt(req.params.id);
-     let ans=operations.find(x=>x.id===paramId);
-     if(!ans){
-         res.status(404).send("Invalid Id");
-     }
-     let op=operations[paramId-1];
-     let sum=0;
-     for(let x of op.operands){
-            sum+=x;
-     }
-     op.result=sum;
-     op.operationPerformed="+";
-     history.push(op);
-    res.send(op);
-
-});
-
-
-router.get('/subtract', (req, res) => {
-    for (let x = 0; x < operations.length; x++) {
-        let arr = operations[x].operands;
-        let minus = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            minus -= arr[i];
-        }
-        operations[x].result = minus;
-        operations[x].operationPerformed="-";
-    }
-    res.send(operations);
-})
-
-router.get('/subtract/:id',(req,res)=>{
-    let paramId= parseInt(req.params.id);
-    let ans=operations.find(x=>x.id===paramId);
-    if(!ans){
-        res.status(404).send("Invalid Id");
-    }
-    let op=operations[paramId-1];
-    let minus=op.operands[0];
-    for(let x=1;x<op.operands.length;x++ ){
-           minus-=op.operands[x];
-    }
-    op.result=minus;
-    op.operationPerformed="-"
-    history.push(op);
-   res.send(op);
-});
-
-
-router.get('/multiply', (req, res) => {
-    for (let x = 0; x < operations.length; x++) {
-        let arr = operations[x].operands;
-        let product = 1;
-        for (let i of arr) {
-            product *= i;
-        }
-        operations[x].result = product;
-        operations[x].operationPerformed="*";
-    }
-    res.send(operations);
-})
-
-router.get('/multiply/:id',(req,res)=>{
-    let paramId= parseInt(req.params.id);
-    let ans=operations.find(x=>x.id===paramId);
-    if(!ans){
-        res.status(404).send("Invalid Id");
-    }
-    let op=operations[paramId-1];
-    let product=op.operands[0];
-    for(let x=1;x<op.operands.length;x++ ){
-           product*=op.operands[x];;
-    }
-    op.result=product;
-    op.operationPerformed="*"
-    history.push(op);
-   res.send(op);
-});
- 
-
-router.get('/divide', (req, res) => {
-    for (let x = 0; x < operations.length; x++) {
-        let arr = operations[x].operands;
-        let divide = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                console.log("Error cant divide by 0");
-                return;
-            }
-            divide = divide / arr[i];
-        }
-        operations[x].result = divide;
-        operations[x].operationPerformed="/";
-    }
-    res.send(operations);
-})
-
-router.get('/divide/:id',(req,res)=>{
-    let paramId= parseInt(req.params.id);
-    let ans=operations.find(x=>x.id===paramId);
-    if(!ans){
-        res.status(404).send("Invalid Id");
-    }
-    let op=operations[paramId-1];
-    let divide=op.operands[0];
-    for(let x=1;x<op.operands.length;x++ ){
-           divide/=op.operands[x];;
-    }
-    op.result=divide;
-    op.operationPerformed="/"
-    history.push(op);
-   res.send(op);
-});
-
-
-
-router.get('/mod', (req, res) => {
-    for (let x = 0; x < operations.length; x++) {
-        let arr = operations[x].operands;
-        let modulus = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            modulus %= arr[i];
-        }
-        operations[x].result = modulus;
-        operations[x].operationPerformed="%"
-    }
-    res.send(operations);
-
-})
-
-router.get('/mod/:id',(req,res)=>{
-    let paramId= parseInt(req.params.id);
-    let ans=operations.find(x=>x.id===paramId);
-    if(!ans){
-        res.status(404).send("Invalid Id");
-    }
-    let op=operations[paramId-1];
-    let modulus=op.operands[0];
-    for(let x=1;x<op.operands.length;x++ ){
-           modulus%=op.operands[x];;
-    }
-    op.result=modulus;
-    op.operationPerformed="%"
-    history.push(op);
-   res.send(op);
-});
-
-router.get('/history',(req,res)=>{
-        res.send(history);
-});
-router.get('/history/last',(req,res)=>{
-    let last=history.length-1;
-    res.send(history[last]);
-});
-router.get('/history/:id',(req,res)=>{
-     let last=parseInt(req.params.id);
-     if(last>history.length){
-        return  res.status(404).send("Invalid reques!! History not present"); 
-     }
-     res.send(history[last-1]);
-});
-router.post('/', (req, res) => {
+router.post('/add', (req, res) => {
     const operation = {
-        id: findLargest(operations) + 1,
-        operands: req.body.operands,
-        result: 0,
-        operationPerformed: ""
+        operands: req.body.operand,
+        result: 0
     }
-    operations.push(operation);
+
+    let sum = 0;
+    for (let i of operation.operands) {
+        sum += i;
+    }
+    operation.result = sum;
+    history.push(operation)
     res.send(operation);
 })
 
+router.post('/subtract', (req, res) => {
+    console.log("hello I m running");
+    const operation = {
+        operands: req.body.operand,
+        result: 0
+    }
+
+    let minus = operation.operands[0];
+    for (let i = 1; i < operation.operands.length; i++) {
+        minus -= operation.operands[i];
+    }
+    operation.result = minus;
+    history.push(operation)
+    res.send(operation);
+})
+
+
+router.post('/multiply', (req, res) => {
+    const operation = {
+        operands: req.body.operand,
+        result: 0
+    }
+
+    let product = 1;
+    for (let i of operation.operands) {
+        product *= i;
+    }
+    operation.result = product;
+    history.push(operation)
+    res.send(operation);
+})
+
+router.post('/divide', (req, res) => {
+    const operation = {
+        operands: req.body.operand,
+        result: 0
+    }
+
+    let divide = operation.operands[0];
+    for (let i = 1; i < operation.operands.length; i++) {
+        divide /= operation.operands[i];
+    }
+    operation.result = divide;
+    history.push(operation)
+    res.send(operation);
+})
+
+router.post('/modulus', (req, res) => {
+    const operation = {
+        operands: req.body.operand,
+        result: 0
+    }
+
+    let mod = operation.operands[0];
+    for (let i = 1; i < operation.operands.length; i++) {
+        mod %= operation.operands[i];
+    }
+    operation.result = mod;
+    history.push(operation)
+    res.send(operation);
+})
+
+
+router.get('/history', (req, res) => {
+    res.send(history);
+});
+
+
+router.get('/history/last', (req, res) => {
+    let last = history.length - 1;
+    res.send(history[last]);
+});
+
+router.get('/history/:id', (req, res) => {
+    let last = parseInt(req.params.id);
+    if (last > history.length) {
+        return res.status(404).send("Invalid reques!! History not present");
+    }
+    res.send(history[last - 1]);
+});
 
 module.exports = router;
